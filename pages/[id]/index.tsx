acaa7f5f766a3c0ab4009d7d0b9dadf954849dd8
fbre0717 <koh2040@naver.com>
2023-10-15 17:11:14 +0900

    feat : SSG dynamic routes

---
+++ pages/[id]/index.tsx
@@ -0,0 +1,95 @@
+import { useRouter } from "next/router";
+import { GetStaticProps, GetStaticPaths } from "next";
+import Image from "next/image";
+
+export default function user({ user }) {
+  const router = useRouter();
+  const id = router.query.id as string;
+  
+  var source = encodeURIComponent(user.id);
+
+  return (
+    <>
+      <title>{user.name}</title>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          src={`https://yena-first.s3.ap-northeast-2.amazonaws.com/${source}.jpg`}
+          width={150}
+          height={150}
+        />
+        <h1>{user.name}</h1>
+        <h1>{id}</h1>
+      </div>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const users = [
+    {
+      id: "yena.jigumina",
+      name: "Yena",
+    },
+    {
+      id: "zo__glasss",
+      name: "Yuri",
+    },
+    {
+      id: "_chaechae_1",
+      name: "Chaewon",
+    },
+  ];
+
+  // 현재 페이지의 id와 일치하는 사용자를 찾음
+  const user = users.find((u) => u.id === context.params.id);
+
+  return {
+    props: { user }, // will be passed to the page component as props
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { id: "yena.jigumina" } },
+      { params: { id: "zo__glasss" } },
+      { params: { id: "_chaechae_1" } },
+    ],
+    fallback: false,
+  };
+};
+
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   return {
+//     props: {
+//       id: "1",
+//       title: "first",
+//     },
+//   };
+// };
+
+// 예시 2
+// return {
+//   paths: [
+//     {
+//       params: {
+//         id: "1",
+//         title: "first post"
+//       }
+//     },
+//     {
+//       params: {
+//         id: "2",
+//         title: "second post"
+//       }
+//     }
+//   ]
+//   // pages/posts/[id]/[title].tsx라고 가정
+//   // pages/posts/1/first post와 pages/posts/2/second post/를 static으로 생성
+// }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Upload() {
   // 추가할만한 기능 : 사진을 업로드 하면, 기본 이미지에서 방금 올린 이미지로 대체되도록 한다.
   // useEffect와 useState 적당히 사용하면 가능할 것 같다.
   return (
     <>
-      <title>♥예나단장♥</title>
+      <title>♥아이즈원♥</title>
       <div
         style={{
           display: "flex",
@@ -13,7 +14,6 @@ export default function Upload() {
           alignItems: "center",
         }}
       >
-        <h1>♥예나단장♥</h1>
         <Image
           // src={`https://${process.env.NEXT_PUBLIC_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_REGION}.amazonaws.com/%EC%98%88%EB%82%98%EB%B2%A0%EB%A0%88%EB%AA%A8.jpg`}
           src={`https://yena-first.s3.ap-northeast-2.amazonaws.com/%EC%98%88%EB%82%98%EB%B2%A0%EB%A0%88%EB%AA%A8.jpg`}
@@ -22,6 +22,7 @@ export default function Upload() {
         />
         <br></br>
         <div style={{ border: "1px solid black", padding: "10px" }}>
+          <p>사진을 올려주세요</p>
           <p>100MB까지 가능합니다</p>
           <input
             onChange={uploadPhoto}
@@ -29,9 +30,17 @@ export default function Upload() {
             accept="image/png, image/jpeg"
           />
         </div>
+        <br></br>
         <div>
-          <br></br>
-          <br></br>
+          <Link href={`/yena.jigumina`}>
+            <h1>최예나</h1>
+          </Link>
+          <Link href={`/zo__glasss`}>
+            <h1>조유리</h1>
+          </Link>
+          <Link href={`/_chaechae_1`}>
+            <h1>김채원</h1>
+          </Link>
         </div>
       </div>
     </>
